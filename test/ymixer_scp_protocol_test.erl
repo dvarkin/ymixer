17460e75c6b16e527dd623834c98b97a24066af0
dvarkin <gmail.com>
2018-01-26 14:55:56 +0200

    add test for SCP protocol

--- test/ymixer_scp_protocol_test.erl
+++ test/ymixer_scp_protocol_test.erl
@@ -12,7 +12,15 @@ set_channel_mix_level_on_test() ->
     ?assertEqual(<<"set MIXER:Current/InCh/ToMix/Level 0 1 0\n">>, ymixer_scp_protocol:set_channel_mix_level_on(0,1)).
 
 
-response_channel_mix_level_test(Response) -> 
+response_channel_mix_level_test() -> 
     ?assertEqual(#{<<"channel">> => 0, <<"mix">> => 1, <<"volume">> => -32768}, 
                  ymixer_scp_protocol:response_channel_mix_level(<<"OK get MIXER:Current/InCh/ToMix/Level 0 1 -32768\n">>)).
-                 
+        
+get_channel_mix_state_test() ->
+    ?assertEqual(<<"get MIXER:Current/InCh/ToMix/On 0 1 0\n">>, ymixer_scp_protocol:get_channel_mix_state(0, 1)).
+
+set_channel_mix_state_test() ->
+    ?assertEqual(<<"set MIXER:Current/InCh/ToMix/On 0 1 0\n">>, ymixer_scp_protocol:set_channel_mix_state(0, 1, 0)).
+         
+response_channel_mix_state_test() ->
+    ?assertEqual( #{<<"channel">> => 0, <<"mix">> => 1, <<"state">> => 0},  ymixer_scp_protocol:response_channel_mix_state(<<"OK get MIXER:Current/InCh/ToMix/On 0 1 0\n">>)).
