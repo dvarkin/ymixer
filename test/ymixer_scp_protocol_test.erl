ca2e800c5316f13c2fc4fb054cf0063e50b0dc85
dvarkin <gmail.com>
2018-01-26 14:48:18 +0200

    add tests

--- test/ymixer_scp_protocol_test.erl
+++ test/ymixer_scp_protocol_test.erl
@@ -1,6 +1,18 @@
 -module(ymixer_scp_protocol_test).
 
+
 -include_lib("eunit/include/eunit.hrl").
 
-make_command_test() ->
-    ?assertEqual(<<"Command 1 2 3">>, ymixer_scp_protocol:mixer_api("Command ~p ~p ~p", [1,2,3])).
+
+set_channel_mix_level_off_test() ->
+    ?assertEqual(<<"set MIXER:Current/InCh/ToMix/Level 0 1 -32768\n">>, ymixer_scp_protocol:set_channel_mix_level_off(0,1)).
+
+
+set_channel_mix_level_on_test() ->
+    ?assertEqual(<<"set MIXER:Current/InCh/ToMix/Level 0 1 0\n">>, ymixer_scp_protocol:set_channel_mix_level_on(0,1)).
+
+
+response_channel_mix_level_test(Response) -> 
+    ?assertEqual(#{<<"channel">> => 0, <<"mix">> => 1, <<"volume">> => -32768}, 
+                 ymixer_scp_protocol:response_channel_mix_level(<<"OK get MIXER:Current/InCh/ToMix/Level 0 1 -32768\n">>)).
+                 
